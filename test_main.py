40337796f2b2bff38dfd43a2220084b3946c0809
Joao Camilo <jlf.camilo@icloud.com>
2022-03-23 16:59:42 -0400

    testing: modfy base test, remove unused cases.

--- test_main.py
+++ test_main.py
@@ -13,3 +13,8 @@ def test_read_root():
     assert response.json() == {
         "message":"Bem vindo a API do projeto Guia Mei Manaus! Para acessar a documentação desta API assim como suas funcionalidades principais acesse o endereço /docs"
     }
+
+def test_pdf_file_response():
+    f = open("files/guia.pdf", "r")
+    response = client.get('/')
+    assert response.content == f.read()
