a2bffa361b2261f3cc1e555b6eb7916d2dedeff4
Joao Camilo <jlf.camilo@icloud.com>
2022-03-23 02:51:55 -0400

    feat: create home route

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ conf = {
 
 app = FastAPI(**conf)
 
+
+@app.get('/', include_in_schema= False)
+def home():
+    return {
+        "message":"Bem vindo a API do projeto Guia Mei Manaus! Para acessar a documentação desta API\
+        assim como suas funcionalidades principais acesse o endereço /docs"
+    }
+
 @app.get('/guia-pdf', tags=["files"])
 def pdf():
     file_path = "files/guia.pdf"
