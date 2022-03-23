e84f48f4b776c03cbda12bd9eadf17d2a9c96a74
Joao Camilo <jlf.camilo@icloud.com>
2022-03-23 03:11:26 -0400

    feat:create base test project.

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse 
+from fastapi import FastAPI as _FastApi
+from fastapi.responses import FileResponse as _FileResponse 
 
 conf = {
     "title": "Guia Mei Manaus",
     "version":"0.0.1"
 }
 
-app = FastAPI(**conf)
+app = _FastApi(**conf)
 
 
 @app.get('/', include_in_schema= False)
 def home():
     return {
-        "message":"Bem vindo a API do projeto Guia Mei Manaus! Para acessar a documentação desta API\
-        assim como suas funcionalidades principais acesse o endereço /docs"
+        "message":"Bem vindo a API do projeto Guia Mei Manaus! Para acessar a documentação desta API assim como suas funcionalidades principais acesse o endereço /docs"
     }
 
 @app.get('/guia-pdf', tags=["files"])
 def pdf():
     file_path = "files/guia.pdf"
-    return FileResponse(file_path)
+    return _FileResponse(file_path)
